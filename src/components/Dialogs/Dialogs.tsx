470867e19aa522fc341d3526096d4948aeb1c0f3
Faukkirik <kote_33419@mail.ru>
2023-03-31 22:41:57 +0300

    8 commit - 20 (CSS)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import s from "./Dialogs.module.css"
 
-export const Dialogs =()=>{
+export const Dialogs = () => {
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + " " + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sweta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>HI</div>
+                <div className={s.message}>Hi, hi</div>
+                <div className={s.message}>How are you?</div>
+            </div>
         </div>
     )
 }
