59030eddc339a4e3a110e723cc5e1dedaca36c88
Faukkirik <kote_33419@mail.ru>
2023-06-08 10:40:54 +0300

    64 commit - 91 (chrome extensions TEST!!!)

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {musicReducer} from "./music-reducer";
@@ -7,7 +7,7 @@ import {newsReducer} from "./news-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
 import thunkMiddleware from "redux-thunk"
-import { reducer as formReducer } from "redux-form"
+import {reducer as formReducer} from "redux-form"
 import {appReducer} from "./app-reducer";
 
 const reducers = combineReducers({
@@ -22,4 +22,7 @@ const reducers = combineReducers({
     appReducer: appReducer
 });
 export type StoreType = ReturnType<typeof reducers>
-export const store = createStore(reducers, applyMiddleware(thunkMiddleware))
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
